8dafa1a814b782976a32434749ba9b08c05c9bac
ilanojoenardsa-dotcom <ilanojoenardsa@gmail.com>
2025-10-21 14:10:54 +0800

    Add files via upload

--- student_app/db.php
+++ student_app/db.php
@@ -1,3 +1,13 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "school_db");
+$servername = "sql204.infinityfree.com";
+$username   = "if0_40217029";
+$password   = "dj4n3ro0n4lEe";
+$dbname     = "if0_40217029_student_db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 ?>
+
